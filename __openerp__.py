159dde8aa3adbbbd820ea25e6d6359e2e3b8abe0
shurshilov <shurshilov.a@yandex.ru>
2015-04-07 11:34:31 +0800

    load module

---
+++ __openerp__.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Табель",
+
+    'summary': """Табель учета рабочего времени""",
+
+    'description': """
+        Позволяет:
+            - импортировать данные из базы данных парус
+            - подписывать простой электронной подписью
+            - печать и все другие действия с документами
+    """,
+
+    'author': "Artem Shurshilov",
+    'website': "http://www.yourcompany.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/master/openerp/addons/base/module/module_data.xml
+    # for the full list
+    'category': 'Test',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base'],
+
+    # always loaded
+    'data': [
+        # 'security/ir.model.access.csv',
+        #'templates.xml',
+	'views/tabel.xml',
+ 	'views/tabel_workflow.xml',
+	'security/security.xml',
+        'security/ir.model.access.csv',
+
+    ],
+    # only loaded in demonstration mode
+    #'demo': [
+    #   'demo.xml',
+    #],
+}
+
+
